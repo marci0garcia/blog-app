2de0bdcdee277fca7aa1005ba5fb853dcf6a4b82
marci0garcia <marciogarcial412@gmail.com>
2024-03-19 16:36:38 -0400

    Component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,8 +2,8 @@ import Featured from "@/components/featured/Featured";
 import CategoryList from "@/components/categoryList/CategoryList";
 import CardList from "@/components/cardList/CardList";
 import styles from "./homepage.module.css";
-import Link from 'next/link';
 import Menu from "@/components/Menu/Menu";
+
 
 export default function Home() {
   return (
