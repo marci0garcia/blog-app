0c30cffb7388dc9f2747851edfbb168d90adb72a
marci0garcia <marciogarcial412@gmail.com>
2024-03-19 15:42:57 -0400

    Featured List Commit

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,19 +1,41 @@
+"use client";
+
+
 import React from "react";
 import styles from "./authLinks.module.css";
 import Link from "next/link";
+import { useState } from "react";
 
 const AuthLinks = () => {
+
+  const[open, setOpen] = useState(false)
+
   const status = "notautenticated";
 
   return (
     <>
-      {status === "notautenticated" ? (<Link href="/login">Login</Link>) : (
+      {status === "notautenticated" ? (
+      <Link href="/login">Login</Link>
+    ) : (
       <>
-        <Link href="/write">Logout</Link>
-        
-        {/*<span className={styles.link}>Logout</span>*/}
+        <Link href="/write" className={styles.link}>Logout</Link>
+        {<span className={styles.link}>Logout</span>}
       </>
       ) }
+
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <dive className={styles.line}></dive>
+        <dive className={styles.line}></dive>
+        <dive className={styles.line}></dive>
+      </div>
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">HomePage</Link>
+          <Link href="/">Contact</Link>
+          <Link href="/">About</Link>
+
+        </div>
+      )}
     </>
   );
 };
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -12,16 +12,16 @@ const NavBar = () => {
       <div className={styles.social}>
         <Image src="/facebook.png" alt="facebook" width={24} height={24} />
         <Image src="/Instagram.png" alt="instagram" width={24} height={24} />
-        <Image src="/tiktok.png" alt="tiktok" width={24} height={24}/>
+        <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
         <Image src="/youtube.png" alt="youtube" width={24} height={24} />
-        </div>
-        <div className={styles.logo}> Culture-Blog</div>
-        <div className={styles.links}>
-        <ThemeToggle/>
-          <Link href="/">HomePage</Link>
-          <Link href="/">Contact</Link>
-          <Link href="/">About</Link>
-          <AuthLinks />
+      </div>
+      <div className={styles.logo}> Marcio's Blog</div>
+      <div className={styles.links}>
+        <ThemeToggle />
+        <Link href="/" className={styles.links}>HomePage</Link>
+        <Link href="/" className={styles.links}>Contact</Link>
+        <Link href="/" className={styles.links}>About</Link>
+        <AuthLinks />
       </div>
     </div>
   );
