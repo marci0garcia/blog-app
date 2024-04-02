734c8a564ed0cf2c93f1833054dcc84c64483cb3
marci0garcia <marciogarcial412@gmail.com>
2024-04-02 15:12:52 -0400

    Complete Front End

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
+import CardList from "@/components/cardList/CardList"
 import styles from "./blogPage.module.css"
 import Menu from "@/components/Menu/Menu"
-import CardList from '@/components/cardList/CardList'
 
 const BlogPage = () => {
   return (
-    <div className = {styles.container}>
-        <h1 className = {styles.title}>Sytle Blog</h1>
-        <div className = {styles.content}>
+    <div className={styles.container}>
+        <h1 className={styles.title}>Style Blog</h1>
+        <div className={styles.content}>
             <CardList />
             <Menu />
         </div>
